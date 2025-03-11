6683267fe6fe11bc06ac3002bce8c1a2d3afde43
StickOnAStick <nndimidik@gmail.com>
2025-03-10 17:20:34 -0700

    Added Method enum & Basic html page.

---
+++ inc/methods.h
@@ -0,0 +1,27 @@
+
+/*
+    GET
+    POST
+    PATCH
+    DELETE
+
+
+    Extras:
+    HEAD
+    PUT
+    CONNECT
+    OPTIONS
+    TRACE
+*/
+
+enum METHODS {
+    GET,
+    POST,
+    PATCH,
+    DELETE,
+    HEAD, // This & downwards are NOT implemented.
+    PUT,
+    CONNECT,
+    OPTIONS,
+    TRACE
+};
---
+++ main.c
@@ -0,0 +1,14 @@
+
+
+
+
+int main(int argc, char* argv){
+    
+    while(true){
+        // Listen
+
+        // If something, act accordingly.
+    }
+
+    return 0;
+}

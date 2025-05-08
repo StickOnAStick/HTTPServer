013b13826ffac9d0188f6c91bcbb302d86c8f86b
StickOnAStick <StickOnAStick>
2025-05-07 18:04:04 -0700

    Implemented Queue

--- src/queue.c
+++ src/queue.c
@@ -11,4 +11,33 @@ void queue_init(queue_t* q){
 
 void queue_push(queue_t* q, task_t task){
     pthread_mutex_lock(&q->lock);
+
+    if(q->count == QUEUE_CAPACITY){
+        // THROW 504 error
+        fprint(stderr, "Queue full!");
+        exit(EXIT_FAILURE);
+    }
+
+    q->tasks[q->rear] = task;
+    q->rear = (q->rear + 1) % QUEUE_CAPACITY;
+    q->count++;
+
+    pthread_cond_signal(&q->sig);
+    pthread_mutex_unlock(&q->lock);
+}
+
+task_t queue_pop(queue_t* q){
+
+    pthread_mutex_lock(&q->lock);
+
+    while(q->count == 0){
+        pthread_cond_wait(&q->sig, &q->lock);
+    }
+
+    task_t res_task = q->tasks[q->front];
+    q->front = (q->front + 1) % QUEUE_CAPACITY;
+    q->count--;
+    
+    pthread_mutex_unlock(&q->lock);
+    return res_task;
 }
---
+++ src/worker.c
@@ -0,0 +1,20 @@
+
+#include <pthread.h>
+#include <stdio.h>
+#include "queue.h"
+
+void* worker_thread(void* arg) {
+    /*
+        The actual worker threads we create. 
+
+        Recieves a copy of the queue. 
+        Each task is a unique function and arguments this thread is responsible for.
+    */
+    queue_t* queue = (queue_t*)arg;
+
+    while(1) {
+        task_t task = queue_pop(queue);
+        task.function(task.arg);
+    }
+    return NULL;
+}
